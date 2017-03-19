224c0c02d2810449e5caed9017548b244e17c092
Paula Gombar <gombarica@gmail.com>
2017-03-19 23:22:03 +0100

    Implement polynomial multiplication and reduction

--- exercise2_2a.c
+++ exercise2_2a.c
@@ -12,7 +12,7 @@ void print1(unsigned char a)
   for (i = 0; i < 8; i++) {
       printf("%d", !!((a << i) & 0x80));
   }
-  //printf("\n");
+  printf("\n");
 }
 
 void print2(unsigned long long a)
@@ -21,7 +21,7 @@ void print2(unsigned long long a)
   for (i = 0; i < 64; i++) {
       printf("%d", !!((a << i) & 0x8000000000000000));
   }
-  //printf("\n");
+  printf("\n");
 }
 
 void poly8_bitslice(poly8x64 r, const poly8 x[64])
@@ -35,7 +35,7 @@ void poly8_bitslice(poly8x64 r, const poly8 x[64])
         for(i = 0; i < 64; i++) {
             // take the nth bit of a polynomial
             bit = (x[i] >> n) & 1; //(x[i] & (1 << n)) >> n;
-            bucket |= bit;
+            bucket ^= bit;
             if (i != 63) bucket <<= 1;
         }
         r[n] = bucket;
@@ -55,47 +55,74 @@ void poly8x64_unbitslice(poly8 r[64], const poly8x64 x)
             }
             // take the nth bit of a polynomial and put it in r[n]
             bit = (x[i] >> n) & 1;
-            r[63-n] |= bit;
+            r[63-n] ^= bit;
             if (i != 0) r[63-n] <<= 1;
         }
       }
 }
 
 
+int poly_degree(unsigned long long a)
+{
+    int sol = 0;
+    while (a >>= 1) {
+      sol++;
+    }
+    return sol;
+}
+
+
 unsigned long long ull_mul(unsigned long long a, unsigned long long b)
 {
-    unsigned long long sol = 0;
-    int n;
+    unsigned long long reduction = 0x0000000000000022LL;
+    unsigned long long sol = 0, tmp = 0;
+    int n, red_degree = poly_degree(reduction);
 
     for (n = 0; n < 64; n++) {
         if ((b >> n) & 1) {
-            sol |= a << n;
+            tmp ^= a << n;
+        }
+        // If the n-th bit is set and the degree < deg(reduction),
+        // no reduction happes, just add to the result
+        if (((tmp >> n) & 1) && n < red_degree) {
+            sol ^= 1 << n;
+        }
+        // If the n-th bit is set and the degree >= deg(reduction),
+        // reduce by turning off the n-th bit and shifting the result
+        if (((tmp >> n) & 1) && n >= red_degree) {
+            sol ^= (1 << n) ^ (reduction << (n - red_degree));
         }
     }
     return sol;
 }
 
 
-unsigned long long ull_red(unsigned long long a)
-{
-    // x8 + x4 + x3 + x + 1
-    unsigned long long reduction = 0x000000000000011BLL;
-    // if deg(poly) < deg(reduction), nothing happens
-    // if deg(poly) == deg(reduction), remove most significant bit
-    // if deg(poly) > deg(reduction), reduce
-
-
-}
-
 /* reduction polynomial x^8 + x^4 + x^3 + x + 1 */
 void poly8x64_mulmod(poly8x64 r, const poly8x64 a, const poly8x64 b)
 {
-    int i;
-    unsigned long long poly_tmp;
+    unsigned long long poly_tmp, tmp;
+    unsigned long long reduction = 0x0000000000000022LL;
+    int i, n, red_degree = poly_degree(reduction);
 
     for (i = 0; i < 8; i++) {
-        poly_tmp = ull_mul(a[i], b[i]);
-        //print2(poly_tmp); printf("\n");
+
+        poly_tmp = 0; tmp = 0;
+        for (n = 0; n < 64; n++) {
+            if ((b[i] >> n) & 1) {
+                tmp ^= a[i] << n;
+            }
+            // If the n-th bit is set and the degree < deg(reduction),
+            // no reduction happes, just add to the result
+            if (((tmp >> n) & 1) && n < red_degree) {
+                poly_tmp ^= 1 << n;
+            }
+            // If the n-th bit is set and the degree >= deg(reduction),
+            // reduce by turning off the n-th bit and shifting the result
+            if (((tmp >> n) & 1) && n >= red_degree) {
+                poly_tmp ^= (1 << n) ^ (reduction << (n - red_degree));
+            }
+        }
+        r[i] = poly_tmp;
     }
 }
 
@@ -132,6 +159,12 @@ int main()
     a[i] = fgetc(urandom);
     b[i] = fgetc(urandom);
   }
+
+/*  unsigned long long bla1 = 0x0000000000000015LL;
+  unsigned long long bla2 = 0x000000000000000CLL;
+  print2(bla1); print2(bla2);
+  print2(ull_mul(bla1, bla2));
+  return 0;*/
 
   poly8_bitslice(va, a);
   poly8_bitslice(vb, b);
